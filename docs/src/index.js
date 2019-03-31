5413810833990463a0b091ac43c6dca77899536f
Ewa <ewa.szczygielska@onet.pl>
2019-03-31 15:53:09 +0200

    import JS

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,2 +1,7 @@
 console.log('test');
 // Ewa API key - ed35929be75cc2d35d7f745115549c49;
+import('./js/searchbar.js');
+import('./js/current.js');
+import('./js/5days.js');
+import('./js/diagrams.js');
+import('./js/maps.js');
