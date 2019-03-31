e51d89efb1fbc6c1ac20814d43b84a0fdddd2afb
kingaGniedziejko <47779092+kingaGniedziejko@users.noreply.github.com>
2019-03-31 18:30:31 +0200

    Update index.js

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,6 +1,7 @@
 console.log('test');
 // Ewa API key - ed35929be75cc2d35d7f745115549c49;
 // Adam API key - fc68321f36609fbd9dcd3f318740aa8a;
+// Kinga API key - 0e3713180683cf2f0424d6d8a2c2ebe2;
 import('./js/searchbar.js');
 import('./js/current.js');
 import('./js/5days.js');
