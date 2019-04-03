0b8400f407cd70c66f6177c1417e4d959b2f3c92
Ewa <ewa.szczygielska@onet.pl>
2019-04-03 14:01:44 +0200

    webpack fight

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -6,16 +6,5 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.join(__dirname, "dist"),
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-        }]
-    },
-    resolveLoader: {
-        modules: ['node_modules'],
-        extensions: ['.js', '.json'],
-        mainFields: ['loader', 'main']
     }
 };
