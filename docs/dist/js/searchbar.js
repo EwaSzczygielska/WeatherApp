db601cc68d2a81da2539ddc36f304f5ba6649b7d
Ewa <ewa.szczygielska@onet.pl>
2019-04-04 14:34:12 +0200

    changes package

--- docs/dist/js/searchbar.js
+++ docs/dist/js/searchbar.js
@@ -1,2 +1,76 @@
-// let cityID = 
 "use strict";
+
+var city;
+var curcity;
+var dayscity;
+var mapcity;
+
+$(document).ready(function () {
+
+    city = "wroclaw";
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
+        type: "GET",
+        dataType: "jsonp",
+        success: function success(data) {
+            curcity = data;
+        }
+    });
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
+        type: "GET",
+        dataType: "jsonp",
+        success: function success(data) {
+            dayscity = data;
+        }
+    });
+
+    $('#submitCity').click(function () {
+
+        var city1 = $('#city').val();
+
+        if (city1 != '') {
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city1 + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
+                type: "GET",
+                dataType: "jsonp",
+                success: function success(data) {
+                    city = city1;
+                    curcity = data;
+                    console.log(city);
+                    console.log(curcity);
+                    $("#error").html('');
+                },
+                error: function error(data) {
+                    $("#error").html('Wrong name of city');
+                    console.log(city);
+                    console.log(curcity);
+                }
+            });
+
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city1 + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
+                type: "GET",
+                dataType: "jsonp",
+                success: function success(data) {
+                    dayscity = data;
+                    console.log(dayscity);
+                },
+                error: function error(data) {
+                    console.log(dayscity);
+                }
+            });
+        } else {
+            $("#error").html('Wrong name of city');
+            console.log(city);
+            console.log(curcity);
+            console.log(dayscity);
+        }
+    });
+
+    $('#sprawdzenie').click(function () {
+        console.log(city);
+        console.log(curcity);
+        console.log(dayscity);
+    });
+});
