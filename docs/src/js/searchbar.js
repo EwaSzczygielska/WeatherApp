e443e24a0792a8cdb48970867179ec4777873c04
Adam <pelswick100@wp.pl>
2019-04-04 14:24:07 +0200

    city name now is okay

--- docs/src/js/searchbar.js
+++ docs/src/js/searchbar.js
@@ -6,16 +6,14 @@ var mapcity;
 $(document).ready(function () {
 
     city="wroclaw";
-
-    $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/weather?q=wroclaw" + city + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
+                $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
                 type: "GET",
                 dataType: "jsonp",
                 success:function(data){
                     curcity=data;
                 }
             });
-
               $.ajax({
                   url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
                   type: "GET",
@@ -30,33 +28,44 @@ $(document).ready(function () {
         var city1 = $('#city').val();
         
         if (city1 != '') {
-            city=city1;
-            console.log(city);
             $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city1 + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
                 type: "GET",
                 dataType: "jsonp",
                 success:function(data){
+                    city=city1;
                     curcity=data;
+                    console.log(city);
+                    console.log(curcity);
                     $("#error").html('');
                 },
                 error:function(data)
                 {
                     $("#error").html('Wrong name of city');
+                    console.log(city);
+                    console.log(curcity);
                 }
             });
 
               $.ajax({
-                  url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
+                  url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city1 + "&appid=fc68321f36609fbd9dcd3f318740aa8a",
                   type: "GET",
                   dataType: "jsonp",
                   success: function (data) {
                       dayscity = data;
+                      console.log(dayscity);
                   },
+                  error:function(data)
+                  {
+                      console.log(dayscity);
+                  }
               });
 
         } else {
             $("#error").html('Wrong name of city');
+            console.log(city);
+            console.log(curcity);
+            console.log(dayscity);
         }
     })
 
