e4cda7a5261d4a14bd044f7d018fa9831bf31a65
Kinga <gniedziejko.kinga@gmail.com>
2019-04-05 21:22:37 +0200

    Update current.js

--- docs/src/js/current.js
+++ docs/src/js/current.js
@@ -1,5 +1,4 @@
 function currentWeather(data){
-    var appID = "0e3713180683cf2f0424d6d8a2c2ebe2";
 
     var tempVal = document.querySelector('#temp-val');
     var tempUnit = document.querySelector('#temp-unit');
@@ -7,34 +6,29 @@ function currentWeather(data){
     var sky = document.querySelector('#sky');
     var infoList = document.querySelector('#info-list');
 
-    //var url = (`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=${appID}`);
-    //fetch(url)
-       //.then(resp => resp.json())
-        //.then(data => {
-            let temperature = data.main.temp;
-            let description = data.weather[0].description;
-            let pressure = data.main.pressure;
-            let humidity = data.main.humidity;
-            let windSpeed = data.wind.speed;
+    let temperature = data.main.temp;
+    let description = data.weather[0].description;
+    let pressure = data.main.pressure;
+    let humidity = data.main.humidity;
+    let windSpeed = data.wind.speed;
 
-            let feelsLike = 0.045 * (Math.pow(5.27, 0.5) + 10.45 - 0.28 * windSpeed) * (temperature - 33) + 33;
+    let feelsLike = 0.045 * (Math.pow(5.27, 0.5) + 10.45 - 0.28 * windSpeed) * (temperature - 33) + 33;
 
-            tempVal.textContent = Math.round(temperature);
-            sky.textContent = description;
-            infoList.innerHTML =
-            `
-            <div class="row">
-                <li class="col-sm-3">Feels Like ${Math.round(feelsLike)}°C</li>          
-                <li class="col-sm-3">Pressure ${pressure} hPa</li>
-            </div>
-            <div class="row">
-                <li class="col-sm-3">Humidity ${humidity}%</li>
-                <li class="col-sm-3">Wind ${windSpeed} m/s</li>    
-            </div> 
-            `;
-        //});
-   
-//Changing unit
+    tempVal.textContent = Math.round(temperature);
+    sky.textContent = description;
+    infoList.innerHTML =
+    `
+    <div class="row">
+        <li class="col-sm-3">Feels Like ${Math.round(feelsLike)}°C</li>          
+        <li class="col-sm-3">Pressure ${pressure} hPa</li>
+    </div>
+    <div class="row">
+        <li class="col-sm-3">Humidity ${humidity}%</li>
+        <li class="col-sm-3">Wind ${windSpeed} m/s</li>    
+    </div> 
+    `;
+    
+    //Changing unit
     tempUnitChange.click(fun  => {
         var temp = 1*tempVal.textContent;
         var unit = tempUnit.textContent;
