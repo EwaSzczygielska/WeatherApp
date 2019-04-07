18a9093a4e0292e8f1ddb5b98a33d52a64fd0468
Adam <pelswick100@wp.pl>
2019-04-07 02:21:16 +0200

    Added 2 maps

--- docs/src/js/maps.js
+++ docs/src/js/maps.js
@@ -1,7 +1,4 @@
-
-    //https: //a.sat.owm.io/sql/3/"+poludnik+"/"+rownoleznik+"/?appid=9de243494c0b295cca9337e1e96b00e2&overzoom=true&op=rgb&from=modis&select=b1,b4,b3&tabActive=2&order=last  
-    
-    var map = new ol.Map({
+     var map = new ol.Map({
         target: 'map',
         layers: [
             new ol.layer.Tile({
@@ -17,12 +14,10 @@
     var adres;
 
     function currentMap(x,y){
-
-    console.log("To jest long" + x);
-    console.log("To jest lat" + y);
     map.getView().setCenter(ol.proj.transform([x, y], 'EPSG:4326', 'EPSG:3857'));
 
     var long = Math.floor(((x + 180)/45));
+
     var lat;
 if (y < -66)
     lat = 6;
@@ -38,9 +33,6 @@ if (y < -66)
                             lat=1;
                             else
                                 lat=0;
-
-    console.log("To jest long"+long);
-    console.log("To jest lat" +lat);
 
     adres = "https://a.sat.owm.io/sql/3/"+long+"/"+lat+"/?appid=9de243494c0b295cca9337e1e96b00e2&overzoom=true&op=rgb&from=modis&select=b1,b4,b3&tabActive=2&order=last";
     $("#my_image").attr("src", adres);
