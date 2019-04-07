9d0f855751a94730016a342b7c8b1bc1449f2bd9
Adam <pelswick100@wp.pl>
2019-04-07 02:07:11 +0200

    Added sattelite map (must repare one thing)

--- docs/src/js/maps.js
+++ docs/src/js/maps.js
@@ -14,6 +14,34 @@
         })
     });;
     
+    var adres;
+
     function currentMap(x,y){
+
+    console.log("To jest long" + x);
+    console.log("To jest lat" + y);
     map.getView().setCenter(ol.proj.transform([x, y], 'EPSG:4326', 'EPSG:3857'));
-    }
+
+    var long = Math.floor(((x + 180)/45));
+    var lat;
+if (y < -66)
+    lat = 6;
+    else if (y < -40)
+        lat = 5;
+        else if (y < 0)
+                lat = 4;
+                else if (y<40)
+                    lat=3;
+                    else if (y<66)
+                        lat=2;
+                        else if(y<80)
+                            lat=1;
+                            else
+                                lat=0;
+
+    console.log("To jest long"+long);
+    console.log("To jest lat" +lat);
+
+    adres = "https://a.sat.owm.io/sql/3/"+long+"/"+lat+"/?appid=9de243494c0b295cca9337e1e96b00e2&overzoom=true&op=rgb&from=modis&select=b1,b4,b3&tabActive=2&order=last";
+    $("#my_image").attr("src", adres);
+}
